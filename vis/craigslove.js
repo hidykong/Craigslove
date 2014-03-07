3d75408a1a6aecaa0ab5a30d8ebae5ef5f4aa822
hidykong <hidykong@gmail.com>
2014-03-07 00:46:27 -0600

    Cities with varied sizes
    I have separated state.csv from the sample (city).csv
    It was just a lot easier for me to do it this way.
    Ben, if you want to remerge the csv's that works, that's fine with me.
    I just didn't see an easier way of doing this.

--- vis/craigslove.js
+++ vis/craigslove.js
@@ -1,18 +1,25 @@
 var w = 960;
 var h = 500;
 
-  var projection = d3.geo.albersUsa()
+//1 = seeking; 2 = sought; 3 = orientation
+var heatmap = 2;
+
+var projection = d3.geo.albersUsa()
 .translate([w/2, h/2])
   .scale([700]);
 
-  //Define path generator
+//Define path generator
 var path = d3.geo.path()
   .projection(projection);
 
-  //Define quantize scale to sort data values into buckets of color
-  //Coloring is off because I am using a single city's value as max rather than the state's -> will have to change later
+//Define quantize scale to sort data values into buckets of color
+//Coloring is off because I am using a single city's value as max rather than the state's -> will have to change later
 var color = d3.scale.quantize()
   .range(["rgb(237,248,233)","rgb(186,228,179)","rgb(116,196,118)","rgb(49,163,84)","rgb(0,109,44)"]);
+
+var radiusScale = d3.scale.linear()
+        .domain([0, 50])
+        .range([1, 8], 0.05);
 
   //Create SVG element
   var svg = d3.select("body")
@@ -20,19 +27,42 @@ var color = d3.scale.quantize()
   .attr("width", w)
   .attr("height", h);
 
-  d3.csv("data/sample_1.csv", function(data) {
-    color.domain([
-      //use w2m to determine the color for now
-      d3.min(data, function(d){ return d.w2m;}),
-      d3.max(data, function(d){ return d.w2m;})
-      ]);
+  d3.csv("data/sample_state.csv", function(data) {
+
+
+    if (heatmap == 1){  //seeking (women/men)
+      color.domain([
+        d3.min(data, function(d){ return (+d.w2m + +d.w2w)/(+d.m2w + +d.m2m);}),
+        d3.max(data, function(d){ return (+d.w2m + +d.w2w)/(+d.m2w + +d.m2m);})
+        ]);
+    } else if (heatmap == 2){ //sought (women/men)
+      color.domain([
+        d3.min(data, function(d){ return (+d.m2w + +d.w2w)/(+d.m2m + +d.w2m);}),
+        d3.max(data, function(d){ return (+d.m2w + +d.w2w)/(+d.m2m + +d.w2m)})
+        ]);
+    } else if (heatmap == 3){ //orientation (homo/hetero)
+      color.domain([
+        d3.min(data, function(d){ return (+d.m2m + +d.w2w)/(+d.m2w + +d.w2m);}),
+        d3.max(data, function(d){ return (+d.m2m + +d.w2w)/(+d.m2w + +d.w2m)})
+        ]);
+    }
+
 
     d3.json("data/us-states.json", function(json) {
 
       //merge city info and GeoJSON
       for (var i = 0; i < data.length; i++){
-        var dataState = data[i].state;
-        var dataValue = parseFloat(data[i].w2m);
+
+        //if (data.label == "city"){
+          var dataState = data[i].state;
+          if (heatmap == 1){  //seeking (women/men)
+            var dataValue = parseFloat(+data[i].w2m + +data[i].w2w)/(+data[i].m2w + +data[i].m2m);
+          } else if (heatmap == 2){ //sought (women/men)
+            var dataValue = parseFloat(+data[i].m2w + +data[i].w2w)/(+data[i].m2m + +data[i].w2m);
+          } else { //orientation (homo/hetero)
+            var dataValue = parseFloat(+data[i].m2m + +data[i].w2w)/(+data[i].m2w + +data[i].w2m);
+          }
+        //}
 
         //loop through states
         for (var j= 0; j < json.features.length; j++) {
@@ -48,7 +78,7 @@ var color = d3.scale.quantize()
             break;
           }
         }
-      }
+      } //end of merge
 
       svg.selectAll("path")
         .data(json.features)
@@ -63,11 +93,13 @@ var color = d3.scale.quantize()
           } else {
             return "#ccc";
           }
-        });
+        }); //fill
+    }); //end of json
 
+    d3.csv("data/sample_1.csv", function(data2) {
       //we can either separate state.csv or keep everything under city.
       svg.selectAll("circle")
-        .data(data)
+        .data(data2)
         .enter()
         .append("circle")
         .attr("cx", function(d) {
@@ -76,8 +108,10 @@ var color = d3.scale.quantize()
       .attr("cy", function(d) {
         return projection([d.longitude, d.latitude])[1];
       })
-      .attr("r", 5)
+      //+d turns a numeric string, d, to a number
+      .attr("r", function(d) { return radiusScale(+d.w2m  +  +d.w2w  +  +d.m2m  +  +d.m2w) ; })
         .style("opacity", 0.75)
+        .style("stroke-width", 0)
         //color it red when hover -> we can change this later to display the information
         .on("mouseover", function(d){
           d3.select(this)
@@ -88,4 +122,5 @@ var color = d3.scale.quantize()
         .attr("fill", "rgb(0, 0, 200)");
       });
     });
-  }); //end of csv
+
+  }); //end of state.csv
