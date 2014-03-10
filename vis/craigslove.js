c4a492635be6bfe550a15e0ffd1e2c8b4fd6fef4
Ben Seefeldt <BenSeefeldt@gmail.com>
2014-03-10 11:50:31 -0500

    Initial Merge. There is some twaking that will need to be done, but it's starting to come together

--- vis/craigslove.js
+++ vis/craigslove.js
@@ -1,20 +1,46 @@
-var w = 960;
+var w = 1200;
 var h = 500;
 
-//1 = seeking; 2 = sought; 3 = orientation
-var heatmap = 1;
-var logValue = 1;
+var margin = {top: 20, right: 20, bottom: 100, left: 40},
+    width = w - margin.left - margin.right,
+    height = h - margin.top - margin.bottom;
 
-var projection = d3.geo.albersUsa()
+var formatPercent = d3.format(".0%");
+
+var x = d3.scale.ordinal()
+  .rangeRoundBands([0, width*0.25], .1, 1);
+
+var y = d3.scale.linear()
+  .range([height, 0]);
+
+  var xAxis = d3.svg.axis()
+.scale(x)
+  .orient("bottom");
+
+  var yAxis = d3.svg.axis()
+.scale(y)
+  .orient("left");
+
+  var svg = d3.select("body").append("svg")
+  .attr("width", width + margin.left + margin.right)
+  .attr("height", height + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+  //1 = seeking; 2 = sought; 3 = orientation
+  var heatmap = 1;
+  var logValue = 1;
+
+  var projection = d3.geo.albersUsa()
 .translate([w/2, h/2])
   .scale([700]);
 
-//Define path generator
+  //Define path generator
 var path = d3.geo.path()
   .projection(projection);
 
-//Define quantize scale to sort data values into buckets of color
-//Coloring is off because I am using a single city's value as max rather than the state's -> will have to change later
+  //Define quantize scale to sort data values into buckets of color
+  //Coloring is off because I am using a single city's value as max rather than the state's -> will have to change later
 var color1 = d3.scale.linear()
   .range (["rgb(251,20,71)","rgb(0,27,137)"]);
 var color2 = d3.scale.linear()
@@ -22,106 +48,298 @@ var color2 = d3.scale.linear()
 var color3 = d3.scale.linear()
   .range (["rgb(218,179,114)","rgb(113,37,117)"]);
 
-var radiusScale = d3.scale.linear()
-        .domain([0, 2000])
-        .range([2, 5], 0.05);
+  var radiusScale = d3.scale.linear()
+.domain([0, 2000])
+  .range([2, 5], 0.05);
 
   //Create SVG element
-  var svg = d3.select("body")
-  .append("svg")
-  .attr("width", w)
-  .attr("height", h);
 
   d3.csv("data/output_state.csv", function(data) {
 
     d3.json("data/us-states.json", function(json) {
 
+      d3.csv("data/city_output.csv", function(data2) {
 
-      //merge state info and GeoJSON
-      for (var i = 0; i < data.length; i++){
+        //merge state info and GeoJSON
+        for (var i = 0; i < data.length; i++){
 
           var dataState = data[i].state;
           //seeking (women/men)
-            var dataValue1 = parseFloat(+data[i].w4m + +data[i].w4w)/(+data[i].m4w + +data[i].m4m);
+          var dataValue1 = parseFloat(+data[i].w4m + +data[i].w4w)/(+data[i].m4w + +data[i].m4m);
           //sought (women/men)
-            var dataValue2 = parseFloat(+data[i].m4w + +data[i].w4w)/(+data[i].m4m + +data[i].w4m);
+          var dataValue2 = parseFloat(+data[i].m4w + +data[i].w4w)/(+data[i].m4m + +data[i].w4m);
           //orientation (homo/hetero)
-            var dataValue3 = parseFloat(+data[i].m4m + +data[i].w4w)/(+data[i].m4w + +data[i].w4m);
-          
-        //loop through states
-        for (var j= 0; j < json.features.length; j++) {
-          var jsonState = json.features[j].properties.name;
-          //if the State matches, store the value in json state
-          if (dataState == jsonState) {
-            json.features[j].properties.value1 = dataValue1;
-            json.features[j].properties.value2 = dataValue2;
-            json.features[j].properties.value3 = dataValue3;
+          var dataValue3 = parseFloat(+data[i].m4m + +data[i].w4w)/(+data[i].m4w + +data[i].w4m);
 
-            break;
+          //loop through states
+          for (var j= 0; j < json.features.length; j++) {
+            var jsonState = json.features[j].properties.name;
+            //if the State matches, store the value in json state
+            if (dataState == jsonState) {
+              json.features[j].properties.value1 = dataValue1;
+              json.features[j].properties.value2 = dataValue2;
+              json.features[j].properties.value3 = dataValue3;
+
+              break;
+            }
           }
-        }
-      } //end of merge
+        } //end of merge
 
 
-//seeking (women/men)
-      color1.domain([
-        d3.min(data, function(d){ return (+d.w4m + +d.w4w)/(+d.m4w + +d.m4m);}),
-        d3.max(data, function(d){ return (+d.w4m + +d.w4w)/(+d.m4w + +d.m4m);})
-        ]);
-//sought (women/men)
-      color2.domain([
-        d3.min(data, function(d){ return (+d.m4w + +d.w4w)/(+d.m4m + +d.w4m);}),
-        d3.max(data, function(d){ return (+d.m4w + +d.w4w)/(+d.m4m + +d.w4m);})
-        ]);
-//orientation (homo/hetero)
-      color3.domain([
-        d3.min(data, function(d){ return (+d.m4m + +d.w4w)/(+d.m4w + +d.w4m);}),
-        d3.max(data, function(d){ return (+d.m4m + +d.w4w)/(+d.m4w + +d.w4m);})
-        ]);
+        //seeking (women/men)
+        color1.domain([
+            d3.min(data, function(d){ return (+d.w4m + +d.w4w)/(+d.m4w + +d.m4m);}),
+            d3.max(data, function(d){ return (+d.w4m + +d.w4w)/(+d.m4w + +d.m4m);})
+            ]);
+        //sought (women/men)
+        color2.domain([
+            d3.min(data, function(d){ return (+d.m4w + +d.w4w)/(+d.m4m + +d.w4m);}),
+            d3.max(data, function(d){ return (+d.m4w + +d.w4w)/(+d.m4m + +d.w4m);})
+            ]);
+        //orientation (homo/hetero)
+        color3.domain([
+            d3.min(data, function(d){ return (+d.m4m + +d.w4w)/(+d.m4w + +d.w4m);}),
+            d3.max(data, function(d){ return (+d.m4m + +d.w4w)/(+d.m4w + +d.w4m);})
+            ]);
 
-    //this is the section we have to refactor to change later
-      svg.selectAll("path")
-        .data(json.features)
-        .enter()
-        .append("path")
-        .attr("d", path)
-        .style("fill", function(d){
-          var value = d.properties.value1;
+        //this is the section we have to refactor to change later
+        svg.selectAll("path")
+          .data(json.features)
+          .enter()
+          .append("path")
+          .attr("d", path)
+          .on("click",function(d){return change("state",d.properties.name);})
+          .style("fill", function(d){
+            var value = d.properties.value1;
 
-          if (value) {
-            return color1(value);
-          } else {
-            return "#ccc";
-          }
-        }); //fill
+            if (value) {
+              return color1(value);
+            } else {
+              return "#ccc";
+            }
+          });//fill
 
 
-    d3.csv("data/city_output.csv", function(data2) {
-      //we can either separate state.csv or keep everything under city.
-      svg.selectAll("circle")
-        .data(data2)
-        .enter()
-        .append("circle")
-        .attr("cx", function(d) {
-          return projection([d.lon, d.lat])[0];
+
+        //we can either separate state.csv or keep everything under city.
+        svg.selectAll("circle")
+          .data(data2)
+          .enter()
+          .append("circle")
+          .attr("cx", function(d) {
+            return projection([d.lon, d.lat])[0];
+          })
+        .attr("cy", function(d) {
+          return projection([d.lon, d.lat])[1];
         })
-      .attr("cy", function(d) {
-        return projection([d.lon, d.lat])[1];
-      })
-      //+d turns a numeric string, d, to a number
-      .attr("r", function(d) { return radiusScale(+d.w4m  +  +d.w4w  +  +d.m4m  +  +d.m4w) ; })
-        .style("opacity", 0.75)
-        .style("stroke-width", 0)
-        //color it red when hover -> we can change this later to display the information
-        .on("mouseover", function(d){
+        //+d turns a numeric string, d, to a number
+        .attr("r", function(d) { return radiusScale(+d.w4m  +  +d.w4w  +  +d.m4m  +  +d.m4w) ; })
+          .style("opacity", 0.75)
+          .style("stroke-width", 0)
+          //color it red when hover -> we can change this later to display the information
+          .on("mouseover", function(d){
+            d3.select(this)
+            .attr("fill", "yellow");
+          })
+        .on("mouseout", function(d){
           d3.select(this)
-          .attr("fill", "yellow");
-        })
-      .on("mouseout", function(d){
-        d3.select(this)
-        .attr("fill", "white");
-      });
-    }); //end of city.csv
+          .attr("fill", "white");
+        });
+
+        // buttons         
+        var seeking = d3.select("#seeking")
+          .on("click", function () {
+            heatmap = 1;
+            drawMap();
+          });
+        var sought = d3.select("#sought")
+          .on("click", function () {
+            heatmap = 2;
+            drawMap();
+          });
+        var orient = d3.select("#orient")
+          .on("click", function () {
+            heatmap = 3;
+            drawMap();
+          });
+
+
+        //check for circle clicks
+        var drawMap = function() {
+
+
+
+          if (heatmap == 1) {
+            svg.selectAll("path")
+              .transition()
+              .duration(750)
+              .ease("linear")
+              .style("fill", function(d){
+                var value = d.properties.value1;
+                if (value) {
+                  logValue = value;
+                  return color1(value);
+                } else {
+                  return "#ccc";
+                }
+
+              }); //fill
+          } else if (heatmap == 2) {
+            svg.selectAll("path")
+              .transition()
+              .duration(750)
+              .ease("linear")
+              .style("fill", function(d){
+                var value = d.properties.value2;
+                if (value) {
+                  logValue = value;
+                  return color2(value);
+                } else {
+                  return "#ccc";
+                }
+
+              }); //fill
+          } else if (heatmap == 3) {
+            svg.selectAll("path")
+              .transition()
+              .duration(750)
+              .ease("linear")
+              .style("fill", function(d){
+                var value = d.properties.value3;
+                if (value) {
+                  logValue = value;
+                  return color3(value);
+                } else {
+                  return "#ccc";
+                }
+
+              }); //fill
+          }
+        };
+        // -- HIDY CODE END
+        // -- BEN CODE BEGIN
+
+
+        //city_list = [];
+
+        //for ( i in data ){
+        //  if ( data[i]['label'] == "city" ) {
+        //    city_list.push({"name":data[i]["city"]});
+        //  }
+        //  //if (data[i]['city'] == "Ludlow") {
+        //  //ob = data[i];
+        //  //}
+        //}
+
+        xDom = ["m4m","m4w","w4m","w4w"];
+        yvals = [ {"tag":"m4m","val":0},
+              {"tag":"m4w","val":0},
+              {"tag":"w4m","val":0},
+              {"tag":"w4w","val":0} ];
+
+
+
+        data.forEach(function(d) {
+          d.frequency = +d.frequency;
+        });
+        var mmv = d3.max(yvals, function(d) { return parseInt(d.val,10); })
+          x.domain(xDom);
+        y.domain([0,20]);
+
+        svg.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxis);
+
+        svg.append("g")
+          .attr("class", "yaxis")
+          .call(yAxis)
+          .append("text")
+          .attr("transform", "rotate(-90)")
+          .attr("y", 6)
+          .attr("dy", ".71em")
+          .style("text-anchor", "end")
+          .text("Frequency");
+
+        svg.selectAll(".bar")
+          .data(yvals)
+          .enter().append("rect")
+          .attr("class", "bar")
+          .attr("x", function(d) { return x(d.tag); })
+          .attr("width", x.rangeBand())
+          .attr("y", function(d) { return y(d.val); })
+          .attr("height", function(d) { return height - y(d.val); });
+
+        //svg.selectAll(".cityButton")
+        //  .data(city_list)
+        //  .enter().append("rect")
+        //  .attr("class","cityButton")
+        //  .attr("x", function(d,i) { return i*40 })
+        //  .attr("width",40)
+        //  .attr("y", height+90)
+        //  .attr("height",10)
+        //  .attr("fill", "blue")
+        //  .on("click", function(d,i) { return change(d["name"]); });
+
+        //d3.select("input").on("change", change);
+
+        //var sortTimeout = setTimeout(function() {
+        //d3.select("input").property("checked", true).each(change);
+        //}, 1000);
+
+        function change(type,name) {
+
+          var ob;
+
+          if ( type == "state" ){
+            for ( i in data ){
+              if (data[i][type] == name) {
+                ob = data[i];
+              }
+            }
+          } else if ( type == "city" ) {
+            for ( i in data2 ){
+              if (data2[i][type] == name) {
+                ob = data2[i];
+              }
+            }
+          }
+
+          yvals = [ {"tag":"m4m","val":ob["m4m"]},
+                {"tag":"m4w","val":ob["m4w"]},
+                {"tag":"w4m","val":ob["w4m"]},
+                {"tag":"w4w","val":ob["w4w"]} ];
+
+          var mmv = d3.max(yvals, function(d) { return parseInt(d.val,10); })
+            x.domain(xDom);
+          y.domain([0,mmv]);
+
+
+          // Copy-on-write since tweens are evaluated after a delay.
+          var x0 = x.domain(data.sort(this.checked
+                ? function(a, b) { return b.frequency - a.frequency; }
+                : function(a, b) { return d3.ascending(a.letter, b.letter); })
+              .map(function(d) { return d.letter; }))
+            .copy();
+
+          var transition = svg.transition().duration(1000),
+              delay = function(d, i) { return i * 50; };
+
+          //transition.selectAll(".bar")
+          //.delay(delay)
+          //.attr("height", function(d) { return height - y(d.val); });
+
+          svg.selectAll(".bar")
+            .data(yvals)
+            .transition()
+            .duration(1000)
+            .attr("height", function(d) { return height - y(d.val); })
+            .attr("y", function(d) { return y(d.val); })
+            svg.select(".yaxis").transition().duration(1000).call(yAxis);
+
+        }
+
+      }); //end of city.csv
 
     }); //end of json = state path
 
@@ -129,73 +347,3 @@ var radiusScale = d3.scale.linear()
   }); //end of state.csv
 
 
-// buttons         
-var seeking = d3.select("#seeking")
-  .on("click", function () {
-      heatmap = 1;
-      drawMap();
-  });
-var sought = d3.select("#sought")
-  .on("click", function () {
-      heatmap = 2;
-      drawMap();
-  });
-var orient = d3.select("#orient")
-.on("click", function () {
-      heatmap = 3;
-      drawMap();
-  });
-
-
-//check for circle clicks
-      var drawMap = function() {
-
-
-
-        if (heatmap == 1) {
-          svg.selectAll("path")
-            .transition()
-            .duration(750)
-            .ease("linear")
-            .style("fill", function(d){
-              var value = d.properties.value1;
-              if (value) {
-                logValue = value;
-                return color1(value);
-              } else {
-                return "#ccc";
-              }
-
-              }); //fill
-          } else if (heatmap == 2) {
-          svg.selectAll("path")
-            .transition()
-            .duration(750)
-            .ease("linear")
-            .style("fill", function(d){
-              var value = d.properties.value2;
-              if (value) {
-                logValue = value;
-                return color2(value);
-              } else {
-                return "#ccc";
-              }
-
-              }); //fill
-          } else if (heatmap == 3) {
-          svg.selectAll("path")
-            .transition()
-            .duration(750)
-            .ease("linear")
-            .style("fill", function(d){
-              var value = d.properties.value3;
-              if (value) {
-                logValue = value;
-                return color3(value);
-              } else {
-                return "#ccc";
-              }
-
-              }); //fill
-          }
-        };
